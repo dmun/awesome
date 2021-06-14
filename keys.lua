4398ca3a667a01225e78eb10bd460ca47332a811
David van Munster <davidru01@live.nl>
2021-06-14 23:12:03 +0200

    Remove unused keymaps

--- keys.lua
+++ keys.lua
@@ -1,6 +1,5 @@
 local gears = require("gears")
 local awful = require("awful")
-local hotkeys_popup = require("awful.hotkeys_popup")
 
 -- Default modkey.
 modkey = "Mod4"
@@ -8,81 +7,7 @@ modkey = "Mod4"
 -- {{{ Key bindings
 globalkeys = gears.table.join()
 
-clientkeys =
-    gears.table.join(
-    awful.key(
-        {modkey, "Shift"},
-        "f",
-        function(c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {description = "toggle fullscreen", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "q",
-        function(c)
-            c:kill()
-        end,
-        {
-            description = "close",
-            group = "client"
-        }
-    ),
-    awful.key(
-        {modkey, "Shift"},
-        "space",
-        awful.client.floating.toggle,
-        {description = "toggle floating", group = "client"}
-    ),
-    awful.key(
-        {modkey, "Control"},
-        "Return",
-        function(c)
-            c:swap(awful.client.getmaster())
-        end,
-        {description = "move to master", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "o",
-        function(c)
-            c:move_to_screen()
-        end,
-        {
-            description = "move to screen",
-            group = "client"
-        }
-    ),
-    awful.key(
-        {modkey},
-        "t",
-        function(c)
-            c.ontop = not c.ontop
-        end,
-        {description = "toggle keep on top", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "m",
-        function(c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end,
-        {description = "minimize", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "f",
-        function(c)
-            c.maximized = not c.maximized
-            c:raise()
-        end,
-        {description = "(un)maximize", group = "client"}
-    )
-)
+clientkeys = gears.table.join()
 
 clientbuttons =
     gears.table.join(
