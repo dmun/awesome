ec7ea6d40f5c5433286d6afdae408d220f2dee68
David van Munster <davidru01@live.nl>
2021-07-14 21:29:19 +0200

    Change local

--- status/memory.lua
+++ status/memory.lua
@@ -3,16 +3,14 @@ local awful = require("awful")
 local interval = 10
 
 awful.widget.watch("cat /proc/meminfo", interval, function(_, stdout)
-    local memory
+    local mem_total = tonumber(stdout:match("MemTotal: *(%d*) kB"))
+    local shmem = tonumber(stdout:match("Shmem: *(%d*) kB"))
+    local mem_free = tonumber(stdout:match("MemFree: *(%d*) kB"))
+    local buffers = tonumber(stdout:match("Buffers: *(%d*) kB"))
+    local cached = tonumber(stdout:match("Cached: *(%d*) kB"))
+    local sreclaimable = tonumber(stdout:match("SReclaimable: *(%d*) kB"))
 
-    mem_total = tonumber(stdout:match("MemTotal: *(%d*) kB"))
-    shmem = tonumber(stdout:match("Shmem: *(%d*) kB"))
-    mem_free = tonumber(stdout:match("MemFree: *(%d*) kB"))
-    buffers = tonumber(stdout:match("Buffers: *(%d*) kB"))
-    cached = tonumber(stdout:match("Cached: *(%d*) kB"))
-    sreclaimable = tonumber(stdout:match("SReclaimable: *(%d*) kB"))
-
-    memory = (mem_total + shmem - mem_free - buffers - cached - sreclaimable) / 1024
+    local memory = (mem_total + shmem - mem_free - buffers - cached - sreclaimable) / 1024
 
     awesome.emit_signal("status::memory", math.floor(memory))
 end)
