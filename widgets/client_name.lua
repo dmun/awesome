fed42a789dde28cdb6b780baf781c2448b90a74e
David van Munster <davidru01@live.nl>
2021-10-25 17:21:50 +0200

    Fix nil class widget

--- widgets/client_name.lua
+++ widgets/client_name.lua
@@ -11,7 +11,13 @@ end)
 
 client.connect_signal("focus", function(c)
     M.font = beautiful.font
-    M.markup = "<b>" .. c.class .. "</b>"
+    local class_name
+    if c.class == nil then
+        class_name = "Application"
+    else
+        class_name = c.class:sub(1,1):upper() .. c.class:sub(2)
+    end
+    M.markup = "<b>" .. class_name .. "</b>"
 end)
 
 return M
