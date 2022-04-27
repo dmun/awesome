f0571580cd7d09b61677bed8c81c62025bfef883
dmun <davidru01@live.nl>
2022-04-27 18:09:31 +0200

    Refactor utils

--- util/init.lua
+++ util/init.lua
@@ -21,4 +21,24 @@ function M.update_border(c)
     end
 end
 
+function M.change(state)
+    local c = client.focus
+    c.fullscreen = false
+    c.maximized = false
+    c.ontop = false
+    c.floating = false
+    awful.layout.set(awful.layout.suit.tile.right)
+    if state == "floating" or state == "stacking" then
+        c.floating = true
+        c.ontop = true
+    elseif state == "maximized" then
+       awful.layout.set(awful.layout.suit.max)
+    elseif state == "fullscreen" then
+        awful.layout.set(awful.layout.suit.max.fullscreen)
+    end
+    for _,tc in pairs(c.screen.tiled_clients) do
+        M.update_border(tc)
+    end
+end
+
 return M
