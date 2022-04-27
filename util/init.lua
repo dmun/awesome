c77f9f095e2a543929e2b00fc5b5087353d5f27f
dmun <davidru01@live.nl>
2022-04-27 18:03:54 +0200

    Fix borders

--- util/init.lua
+++ util/init.lua
@@ -4,18 +4,18 @@ local beautiful = require("beautiful")
 local M = {}
 
 function M.update_border(c)
-    local s = awful.screen.focused()
+    local s = c.screen
     if #s.tiled_clients == 1
         or s.selected_tag and (s.selected_tag.layout.name == 'max' or s.selected_tag.layout.name == 'fullscreen')
     then
-        awful.screen.padding(c.screen, 0)
+        awful.screen.padding(s, 0)
         if c.floating then
             c.border_width = beautiful.border_width
         else
             c.border_width = 0
         end
     else
-        awful.screen.padding(c.screen, { top = 1, right = 2, bottom = 2, left = 2 })
+        awful.screen.padding(s, { top = 1, right = 2, bottom = 2, left = 2 })
         awful.placement.honor_padding = false
         c.border_width = beautiful.border_width
     end
